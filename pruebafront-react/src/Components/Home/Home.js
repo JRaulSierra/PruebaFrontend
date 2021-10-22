19af43de5bc1550a4c8c103ca0012335b7ac62da
JRaulSierra <70240223+JRaulSierra@users.noreply.github.com>
2021-10-22 16:05:39 -0600

    Eliminar cosas innecesaria

---
+++ pruebafront-react/src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div>
+            <h1>Información actualizada Sobre los casos de COVID-19</h1>
+        </div>
+    );
+}
+
+export default Home;
